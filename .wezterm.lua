82e642051f24f7cfa1c54080cef27b67b0ccf13b
shotarokuramata <shokuramata@gmail.com>
2026-01-31 12:48:05 +0900

    タブバーは常に表示

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ config.window_decorations = "RESIZE"
 -- タブバーの表示
 config.show_tabs_in_tab_bar = true
 -- タブが一つの時は非表示
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
 -- falseにするとタブバーの透過が効かなくなる
 -- config.use_fancy_tab_bar = false
 
